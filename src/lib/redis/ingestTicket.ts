867255182f84f5dbb2a1cbfce87dae25c5e93148
harryob <55142896+harryob@users.noreply.github.com>
2025-12-20 19:09:18 +0000

    fix some lints

--- src/lib/redis/ingestTicket.ts
+++ src/lib/redis/ingestTicket.ts
@@ -11,18 +11,18 @@ export const ingestTicket = async (message: string, channel: string) => {
 
 	const data = JSON.parse(message);
 
-	let author;
+	let author: string | undefined;
 
-	if (data['recipient']) {
-		author = `${data['recipient']}`;
+	if (data.recipient) {
+		author = `${data.recipient}`;
 	}
 
-	if (data['sender']) {
-		author = `${data['sender']}`;
+	if (data.sender) {
+		author = `${data.sender}`;
 	}
 
-	if (data['sender'] && data['recipient']) {
-		author = `${data['sender']} -> ${data['recipient']}`;
+	if (data.sender && data.recipient) {
+		author = `${data.sender} -> ${data.recipient}`;
 	}
 
 	const embedToSend = new EmbedBuilder();
@@ -31,7 +31,7 @@ export const ingestTicket = async (message: string, channel: string) => {
 	embedToSend.setDescription(`${data['message']}`);
 	embedToSend.setColor('DarkAqua');
 	embedToSend.setTimestamp();
-	embedToSend.setFooter({ text: `@${data['source']}` });
+	embedToSend.setFooter({ text: `@${data.source}` });
 
 	channelToUse.send({ embeds: [embedToSend] });
 };
